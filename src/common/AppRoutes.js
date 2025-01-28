8bf703b6702e22ba98c2edc3f6f49226c97f3662
Sahana B G <sahanabg580@gmail.com>
2025-01-28 13:39:56 +0530

    Update AppRoutes.js

--- src/common/AppRoutes.js
+++ src/common/AppRoutes.js
@@ -1,6 +1,6 @@
 export const ApiRoutes = {
-  REGISTER: "http://localhost:1000/api/user/${data}/register",
-  UNREGISTER: "http://localhost:1000/api/user/${data}/unregister",
+  REGISTER: (data) => `http://localhost:1000/api/user/${data}/register`,
+  UNREGISTER: (data) => `http://localhost:1000/api/user/${data}/unregister`,
   CREATE_USER: "http://localhost:1000/api/user/create",
   LOGIN_USER: "http://localhost:1000/api/user/login",
   ALL_EVENTS: "http://localhost:1000/api/events",
